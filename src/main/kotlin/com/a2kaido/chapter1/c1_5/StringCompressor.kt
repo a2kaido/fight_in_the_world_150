34ea6c66e90c97199a0374a704756052bd849798
a2kaido <amane.nikaido+a2kaido@gmail.com>
2018-10-04 21:12:15 +0900

    compressor

---
+++ src/main/kotlin/com/a2kaido/chapter1/c1_5/StringCompressor.kt
@@ -0,0 +1,28 @@
+package com.a2kaido.chapter1.c1_5
+
+fun main(args: Array<String>) {
+    println(compress("aaabbbbccc"))
+    println(compress("abcdefghi"))
+}
+
+fun compress(str: String): String {
+    var i = 0
+    var prev = ""
+    val builder = StringBuilder()
+    for (c in str) {
+        if (c.toString() == prev) {
+            i++
+        } else {
+            if (i > 0) {
+                builder.append(i.toString())
+            }
+            i = 1
+            builder.append(c)
+        }
+
+        prev = c.toString()
+    }
+    builder.append(i.toString())
+    val res = builder.toString()
+    return if (res.length > str.length) str else res
+}
